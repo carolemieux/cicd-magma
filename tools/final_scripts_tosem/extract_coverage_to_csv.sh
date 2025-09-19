8d3db95be011df9ee45195bc9c84f1a564a0ecb7
Caroline Lemieux <caro.lemieux@gmail.com>
2025-09-19 14:10:29 -0700

    more extraction scripts

---
+++ tools/final_scripts_tosem/extract_coverage_to_csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+coverage_dir=$1
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 coverage_results_directory"
+  exit 1
+fi
+
+pushd $coverage_dir
+while IFS=, read -r tech bench; do
+        for cov_file in ${tech}_${bench}_*total_coverage.txt; do
+             tail -n 1 $cov_file | awk '{print $13}' | tr -d '%'
+        done | xargs echo $tech $bench | tr ' ' ','
+done < <(ls -1 | sed 's|\([^_]*\)_\([a-zA-Z0-9]\+_[0-9]\+_[0-9]\+\).*|\1,\2|' | sort | uniq)
+
+popd
