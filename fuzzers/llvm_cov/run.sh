f0680501cd2af498e72bf3228cb94b0792d35dd0
Caroline Lemieux <caro.lemieux@gmail.com>
2025-12-02 11:51:59 -0800

    update coverage scripts to require less manual editing points

--- fuzzers/llvm_cov/run.sh
+++ fuzzers/llvm_cov/run.sh
@@ -9,33 +9,33 @@
 # - env PROGRAM: name of program to run (should be found in $OUT)
 # - env ARGS: extra arguments to pass to the program
 # - env FUZZARGS: extra arguments to pass to the fuzzer
+# - env COVERAGE_FUZZER: fuzzer to collect coverage for
 ##
 
 echo "No fuzzer included. This is just for building an analysis target."
+echo "Collecting coverage for fuzzer ${COVERAGE_FUZZER}."
 
 mkdir -p $SHARED/coverage
 pushd $FUZZER
 
-for i in $(seq 0 9); do 
-    export CORPUS="$SEED/aflgoexp/$PROGRAM/$i/findings/queue"
-    ./collect_coverage.sh $i aflgoexp
+if [ ! -d "$SEED/${COVERAGE_FUZZER}/$PROGRAM" ]; then
+	echo "$SEED/${COVERAGE_FUZZER}/$PROGRAM does not exist; not collecting coverage"
+	popd 
+	exit 1
+fi
 
-    # export CORPUS="$SEED/aflgo/$PROGRAM/$i/findings/queue"
-    # ./collect_coverage.sh $i aflgo
-
-    # export CORPUS="$SEED/afl/$PROGRAM/$i/findings/queue"
-    # ./collect_coverage.sh $i afl
-
-    # export CORPUS="$SEED/ffd/$PROGRAM/$i/findings/queue"
-    # ./collect_coverage.sh $i ffd
-
-    # export CORPUS="$SEED/libfuzzer/$PROGRAM/$i/corpus/seed"
-    # ./collect_coverage.sh $i libfuzzer
-
-    # export CORPUS="$SEED/aflpp/$PROGRAM/$i/findings/default/queue"
-    # ./collect_coverage.sh $i aflpp
-
+for repdir in "$SEED/${COVERAGE_FUZZER}/$PROGRAM/*"; do 
+    i=$(basename $repdir)
+    if [ -d $repdir/findings/queue ]; then    #afl-style
+    	export CORPUS="$SEED/${COVERAGE_FUZZER}/$PROGRAM/$i/findings/queue"
+    	./collect_coverage.sh $i ${COVERAGE_FUZZER}
+    elif [ -d $repdir/findings/default/queue ]; then #aflpp-style
+    	export CORPUS="$SEED/${COVERAGE_FUZZER}/$PROGRAM/$i/findings/default/queue"
+    	./collect_coverage.sh $i ${COVERAGE_FUZZER}
+    else
+	echo "Could not find queue directory; not collecting coverage"
+    fi
 done
-popd
 
+popd
 exit 1
